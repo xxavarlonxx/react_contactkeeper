634df676954276e6a51ead2494bbb1611771c011
andre <ahochschulte.apple@gmail.com>
2020-11-12 19:31:25 +0100

    Express server setup

---
+++ routes/contacts.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+//@route    GET /api/contacts
+//@desc     Get all user contacts
+//@access   private
+router.get('/', (req, res) => {
+  res.send('Get all contacts');
+});
+
+//@route    POST /api/contacts
+//@desc     Add new contact
+//@access   private
+router.post('/', (req, res) => {
+  res.send('Add new contact');
+});
+
+//@route    PUT /api/contacts/:id
+//@desc     Update contact
+//@access   private
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
+});
+
+//@route    DELETE /api/contacts/:id
+//@desc     Delete contact
+//@access   private
+router.delete('/:id', (req, res) => {
+  res.send('Delete contact');
+});
+module.exports = router;
